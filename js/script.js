9c76afd234ce40960c4789cf95dd7c2996ba7a41
Artem <Jaksan@gmail.com>
2021-10-15 16:05:10 +0300

    fixed some errors

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@ let button = document.getElementById('menu-btn');
 
 let navigation = document.getElementById('menu');
 
-let box = document.getElementById('box');
-
 button.addEventListener('click', function () {
     navigation.classList.toggle('display-block');
-    box.classList.toggle('display-block');
 });
