6a6690291df9f96e9b2bc9d2a7da56c23457ac0e
Artem <Jaksan@gmail.com>
2021-11-02 18:38:27 +0300

    hw-7

--- js/script.js
+++ js/script.js
@@ -36,3 +36,33 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
 }
+
+
+
+
+let arrowColor = document.getElementById('arrow1');
+
+let navi = document.getElementById('color');
+
+arrowColor.addEventListener('click', function () {
+    navi.classList.toggle('display-block');
+});
+
+
+
+let arrowSize = document.getElementById('size');
+
+let navig = document.getElementById('arrow2');
+
+arrowSize.addEventListener('click', function () {
+    navig.classList.toggle('display-block');
+});
+
+
+let arrowQuantity = document.getElementById('arrow3');
+
+let naviga = document.getElementById('quantity');
+
+arrowQuantity.addEventListener('click', function () {
+    naviga.classList.toggle('display-block');
+});
